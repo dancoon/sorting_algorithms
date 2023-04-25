d8880cdb569e3d2602e55ca46a0c62023a59d6c2
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-04-25 01:34:25 -0700

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,9 +11,12 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
-	for (i = 0; i < size; i++)
+	if (array == NULL || size == 0)
+		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = size - i - 1; j < size; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
