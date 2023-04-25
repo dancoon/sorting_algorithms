dc4875b4424711c99ecd328016e91c2ccaca1ed8
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-04-25 00:57:03 -0700

    Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of int in ascending order
+ * @array: array of int
+ * @size: size of array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[i])
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
